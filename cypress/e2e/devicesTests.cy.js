c43661fbfd11f12f4ebdfd0b910d678ccbb66559
Alina Dudzic <alina@dudzic.pl>
2023-07-07 01:24:34 +0200

    add asertion

--- cypress/e2e/devicesTests.cy.js
+++ cypress/e2e/devicesTests.cy.js
@@ -8,15 +8,17 @@ describe('add new device and sensors', () => {
     cy.get('#name').type('device1')
     cy.contains('button', 'Confirm').click()
     cy.contains('p', 'device1');
+    cy.get('a[href="/device/509fed1c-1ec3-480f-9675-ba0fbd261b00"]').click();
+    cy.url('https://diagla.vot.pl/device/509fed1c-1ec3-480f-9675-ba0fbd261b00');
     })
 
     it('add sensor', function () {
-    cy.get('a[href="/device/509fed1c-1ec3-480f-9675-ba0fbd261b00"]').click();
-    cy.get('svg[data-testid="AddIcon"]').click();
-    cy.get('#name').type('sensor1')
-    cy.contains('button', 'Confirm').click()
+        cy.get('svg[data-testid="AddIcon"]').click();
+    cy.get('#name').type('sensor1');
+    cy.contains('button', 'Confirm').click();
     cy.contains('div', 'sensor1');
-    cy.get('a[href="/sensor/13983e07-5379-4e85-8a27-93fcf26a5ebf"]')
+    cy.get('a[href="/sensor/13983e07-5379-4e85-8a27-93fcf26a5ebf"]');
+    cy.url('https://diagla.vot.pl/sensor/13983e07-5379-4e85-8a27-93fcf26a5ebf');
     cy.logout();
     })
 })
